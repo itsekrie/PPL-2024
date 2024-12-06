7af6d150ac41ec42b3b5ca8b8ba5e17b3a83cb52
Yusuf Zaenul Mustofa <yusufmustofa930@gmail.com>
2024-12-06 16:56:28 +0700

    update ruangan status

--- lib/Ruangan/Services/RuanganService.dart
+++ lib/Ruangan/Services/RuanganService.dart
@@ -6,9 +6,11 @@ class RuangKuliah {
   String nama;
   int kapasitas;
   String departemen;
+  String status; 
 
-  RuangKuliah(this.no, this.gedung, this.nama, this.kapasitas, this.departemen);
+  RuangKuliah(this.no, this.gedung, this.nama, this.kapasitas, this.departemen, this.status);
 }
+
 
 class RuangService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -23,6 +25,7 @@ class RuangService {
         data['nama'],
         data['kapasitas'],
         data['departemen'],
+        data['status'] ?? 'belum_diajukan',
       );
     }).toList();
   }
@@ -33,19 +36,37 @@ class RuangService {
       'nama': nama,
       'kapasitas': kapasitas,
       'departemen': departemen,
+      'status' : 'belum_diajukan',
     });
   }
 
-  Future<void> editRuang(String id, String gedung, String nama, int kapasitas, String departemen) async {
+  Future<void> editRuang(String id, String gedung, String nama, int kapasitas, String departemen, String status) async {
     await _firestore.collection('Ruang').doc(id).update({
       'gedung': gedung,
       'nama': nama,
       'kapasitas': kapasitas,
       'departemen': departemen,
+      'status': status,
     });
   }
 
   Future<void> deleteRuang(String id) async {
     await _firestore.collection('Ruang').doc(id).delete();
   }
+
+  Future<void> ajukanSemuaRuang() async {
+    final querySnapshot = await _firestore
+        .collection('Ruang')
+        .where('status', isEqualTo: 'belum_diajukan') // Filter berdasarkan status
+        .get();
+
+    final batch = _firestore.batch();
+
+    for (final doc in querySnapshot.docs) {
+      batch.update(doc.reference, {'status': 'diajukan'});
+    }
+
+    await batch.commit(); // Jalankan update dalam satu transaksi batch
+  }
+
 }
